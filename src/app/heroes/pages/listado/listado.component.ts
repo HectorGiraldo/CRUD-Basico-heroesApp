e9c6f453f7da93249e440a1b1b12a5564e725c3c
Hector <hectorgiraldo0210@gmail.com>
2022-02-07 21:40:48 +0100

    CRUD basico

---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styles: [
+  ]
+})
+export class ListadoComponent implements OnInit {
+
+  heroes : Heroe[] = []
+
+  constructor(
+    private heroesServices: HeroesService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.heroesServices.getHeroes()
+        .subscribe( (heroes:Heroe[]) => {
+          this.heroes = heroes
+        });
+  }
+
+  
+
+}
